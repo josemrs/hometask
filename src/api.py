290e02cc9c1aa5fc44ef48c5154afbcd7bc343e0
José María Rodríguez Sáez <jmrs@protonmail.com>
2024-07-19 18:30:59 +0200

    black happy

--- src/api.py
+++ src/api.py
@@ -7,20 +7,22 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 
 from models import (
-#    Base,
+    #    Base,
     User,
     PutDateOfBirth,
     GetUsernameResponse,
 )  # pylint: disable=import-error
-#from db import engine, get_db  # pylint: disable=import-error
+
+# from db import engine, get_db  # pylint: disable=import-error
 from db import get_db  # pylint: disable=import-error
 
 api = FastAPI()
 
 # Initialize the database (Redundant if using migrations)
-#Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 Instrumentator().instrument(api).expose(api)
+
 
 @api.put("/hello/{username}", status_code=204)
 async def create_user(
